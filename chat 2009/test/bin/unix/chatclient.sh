5ee6875c289492647fc547e4fa2dd7a228e98d91
ltn <lighteningman@gmail.com>
2013-02-03 17:41:16 +0100

    CHAT is running now

--- chat 2009/test/bin/unix/chatclient.sh
+++ chat 2009/test/bin/unix/chatclient.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/env sh
 # 07-feb-2008/FK Added USER env variable to HTTP value.
 # -- ----------------------------------------------------------------
 # -- This file is for Unix systems.
@@ -7,7 +7,7 @@
 # -- is not in *your* public_html (web access) directory.
 # -- ----------------------------------------------------------------
 
-HTTP=http://www.dsv.su.se/~${USER}/prislab1/client
+HTTP=http://localhost:8080/client
 
 ROOT=$(dirname $0)/../..
 
